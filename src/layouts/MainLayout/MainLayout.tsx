99295469de7152d7576e49faffef89232841b4ce
4nh3k <21520579@gm.uit.edu.vn>
2023-12-28 18:01:22 +0700

    Refactor Sidebar component and add user avatar dropdown menu

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -1,15 +1,36 @@
+import { Avatar, Dropdown } from 'flowbite-react';
 import { Outlet } from 'react-router-dom';
 import Breadcrumbs from 'src/components/Breadcrumb';
 import SidebarComponent from 'src/components/Sidebar';
 
 export default function MainLayout() {
   return (
-    <div className='min-h-screen w-screen overflow-y-hidden'>
-      <SidebarComponent />
-      <div className='ml-auto min-h-screen w-[calc(100%-6rem)] overflow-y-auto bg-gray-100 lg:w-[calc(100%-15rem)]'>
+    <div className='flex h-screen w-screen overflow-y-hidden'>
+      <div className='z-10 h-screen lg:z-0 '>
+        <SidebarComponent />
+      </div>
+      <div className='ml-auto min-h-screen w-full overflow-y-auto bg-gray-100 '>
         {/* <Header /> */}
-        <div id='main-body' className='space-y-5 p-10'>
-          <Breadcrumbs />
+        <div id='main-body' className='space-y-5 px-10 pb-10 pt-5'>
+          <div className='flex items-end'>
+            <Breadcrumbs />
+            <div className='ml-auto'>
+              <Dropdown
+                label={
+                  <Avatar
+                    img='https://ui-avatars.com/api/?background=0D8ABC&color=fff'
+                    rounded
+                  />
+                }
+                inline
+              >
+                <Dropdown.Item>Dashboard</Dropdown.Item>
+                <Dropdown.Item>Settings</Dropdown.Item>
+                <Dropdown.Item>Earnings</Dropdown.Item>
+                <Dropdown.Item>Sign out</Dropdown.Item>
+              </Dropdown>
+            </div>
+          </div>
           <Outlet />
         </div>
       </div>
