ecda20de411fe31ba4f593107fdd25a5d8c0975b
4nh3k <21520579@gm.uit.edu.vn>
2023-12-28 10:54:19 +0700

    Refactor Pagination component CSS and add wrapper div

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -12,18 +12,20 @@ export default function Pagination({
   handlePageClick
 }: PaginationProps) {
   return (
-    <ReactPaginate
-      className='flex items-center justify-center space-x-2 text-sm'
-      pageClassName='border-primary h-8 w-8 border bg-white text-center '
-      pageLinkClassName='h-full w-full flex items-center justify-center'
-      breakLabel='...'
-      nextLabel='Trang sau'
-      initialPage={0}
-      onPageChange={handlePageClick}
-      pageRangeDisplayed={pageRangeDisplayed}
-      pageCount={pageCount}
-      previousLabel='Trang trước'
-      renderOnZeroPageCount={null}
-    />
+    <div className='pagination'>
+      <ReactPaginate
+        className='flex items-center justify-center space-x-2 text-sm'
+        pageClassName='border-primary h-8 w-8 border bg-white text-center '
+        pageLinkClassName='h-full w-full flex items-center justify-center'
+        breakLabel='...'
+        nextLabel='Trang sau'
+        initialPage={0}
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={pageRangeDisplayed}
+        pageCount={pageCount}
+        previousLabel='Trang trước'
+        renderOnZeroPageCount={null}
+      />
+    </div>
   );
 }
