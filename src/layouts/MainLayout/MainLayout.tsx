c3f956c44cb2b2ed210dc8a64afcb20dfee70533
4nh3k <21520579@gm.uit.edu.vn>
2023-12-28 10:39:03 +0700

    Add icons to sidebar items and update layout

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom';
+import Header from 'src/components/Header/Header';
 import SidebarComponent from 'src/components/Sidebar/Sidebar';
 
 export default function MainLayout() {
   return (
     <div className='h-screen w-screen overflow-y-hidden'>
       <SidebarComponent />
-      <div className='ml-auto h-screen w-[calc(100%-4.2rem)] overflow-y-auto bg-gray-100 lg:w-[calc(100%-9rem)]'>
+      <div className='ml-auto h-screen w-[calc(100%-6rem)] overflow-y-auto bg-gray-100 lg:w-[calc(100%-15rem)]'>
+        <Header />
         <Outlet />
       </div>
     </div>
--- src/pages/Student/AllStudent.tsx
+++ src/pages/Student/AllStudent.tsx
@@ -4,7 +4,6 @@ import Header from 'src/components/Header/Header';
 const AllStudent = () => {
   return (
     <div className=''>
-      <Header></Header>
       <div id='main-body'>ABCXYZ</div>
     </div>
   );
